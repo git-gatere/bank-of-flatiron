44ce3f429da4255cca0ba009f72ec801c7e54372
gatere <mercynjerigatere@gmail.com>
2024-04-29 01:04:23 -0400

    Adds more changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
-import SearchBar from "./components/Searchbar";
-import Title from "./components/Title"
+import React, { useState } from 'react';
+import './App.css';
+import Form from './components/Form';
+import SearchBar from './components/Searchbar'; 
 
-function App () {
-  return <>
-  <Title/>
-  <SearchBar/>
-  <Form/>
-  </>
-};
+
+function App() {
+  const [transactions, setTransactions] = useState([]);
+  const addTransaction = (transaction) => {
+    setTransactions([...transactions, transaction]);
+  };
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return (
+    <div className="container"> {/* Container to span the whole width of the page */}
+      <h1 className="App">The Royal Bank of Flatiron</h1>
+      <SearchBar value={searchTerm} onChange={(value) => setSearchTerm(value)} /> {/* Pass onChange prop */}
+      <Form addTransaction={addTransaction} />
+      
+    </div>
+  );
+}
 
 export default App;
